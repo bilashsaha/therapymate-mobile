c5499a9bc38da2a5d71e1cfb095047077903e326
Bilash Saha <bilashsaha03@gmail.com>
2019-08-05 04:47:14 -0600

    - added es6 support

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,22 @@ var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var babel = require("gulp-babel");
+var plumber = require("gulp-plumber");
 var sh = require('shelljs');
 
 var paths = {
+  es6: ['./src/es6/*.js'],
   sass: ['./scss/**/*.scss']
 };
+
+gulp.task("babel", function () {
+  return gulp.src(paths.es6)
+    .pipe(plumber())
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(gulp.dest("www/js"));
+});
+
 
 gulp.task('default', ['sass']);
 
@@ -27,6 +38,7 @@ gulp.task('sass', function(done) {
 });
 
 gulp.task('watch', ['sass'], function() {
+  gulp.watch(paths.es6, ['babel']);
   gulp.watch(paths.sass, ['sass']);
 });
 
